e88a5f089e03539dc45bf9d48c179fc9fa6eb2ca
Taras <taras18taras@gmail.com>
2018-05-27 15:46:53 +0300

    user

---
+++ user/index.js
@@ -0,0 +1,12 @@
+var phrases = require ('./ru');
+
+function Index(name) {
+    this.name = name;
+}
+
+Index.prototype.hello = function (who) {
+    console.log( phrases.Hello + ", " + who.name);
+};
+
+console.log("user.js is required!")
+exports.User = Index;
